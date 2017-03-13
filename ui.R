115f7a2b03fdcbbd39d96820c0877dddaae7203c
Chris <chrisvwn@yahoo.co.uk>
2017-03-13 11:51:58 +0300

    +removed the btnDraw +renamed btnCtry to btnGo. Note map will also be dependent on this button now

--- ui.R
+++ ui.R
@@ -102,10 +102,10 @@ alignCenter <- function(el) {
                   tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
                   leafletOutput("map"),
                   
-                  uiOutput("sliderNlYearMonth"),
+                  uiOutput("sliderNlYearMonth")
                   
-                  actionButton(inputId="drawMap",
-                               label = "Draw Map")
+#                   actionButton(inputId="drawMap",
+#                                label = "Draw Map")
                   )
 # 
 #           tabItem(tabName = "stats",
